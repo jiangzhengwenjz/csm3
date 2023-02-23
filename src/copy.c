d80471e9741dcbbe74e54b6e94080dce7a99aa3a
Zhengwen Jiang <jian0463@umn.edu>
2023-02-22 22:51:42 -0600

    fix gUnk_030040C0

--- src/copy.c
+++ src/copy.c
@@ -20,39 +20,33 @@ void DmaCopyBufferToOam(void)
 void sub_080092EC(void)
 {
     CpuFastFill(0x2e4, gOamBuffer, 0x400);
-    CpuFill32(-1,gUnk_030037A0,0x10);
+    CpuFill32(-1, gUnk_030037A0, 0x10);
     gUnk_03003CB0[0] = 0;
     gUnk_03003CB0[2] = 0;
 }
 
-void sub_08009334(u8 a1, u32 *a2, u32 a3, u32 a4)
+void sub_08009334(u8 a1, u32 *a2, u32 tilesVram, u32 a4)
 {
-    u16 *v1;
-    u32 *v2;
-    v1 = gUnk_030040C0 + a1 * 8;  //TODO:I think gUnk_030040C0 is a struct, but it is wierd to find members through a variable. 
-    v2 = (u32*)v1;
-    v1[0] = a2[0];
-    v1[1] = (a3 + 0xf9ff0000) / 32;
-    v2[1] = a2[4];
-    v2[2] = a2[6];
-    v2[3] = a4;
+    struct Unk_030040C0 *var = &gUnk_030040C0[a1];
+
+    var->unk0 = a2[0];
+    var->unk2 = (tilesVram - 0x6010000) / 32; // 4BPP
+    var->unk4 = a2[4];
+    var->unk8 = a2[6];
+    var->unkC = a4;
 }
 
 void sub_08009364(u8 a1, u16 a2, u32 a3)
 {
-    u16 *v1;
-    u32 *v2;
-    v1 = gUnk_030040C0 + a1 * 8;  //TODO:Same above.I tried struct array type,but it doesn't match.
-    v2 = (u32*)v1;
-    v1[0] = a2;
-    v2[2] = a3;
+    struct Unk_030040C0 *var = &gUnk_030040C0[a1];
+
+    var->unk0 = a2;
+    var->unk8 = a3;
 }
 
 u32 sub_08009378(u8 a1, u16 a2, u32 a3)
 {
-    u16 *v1;
-    u32 *v2;
-    v1 = gUnk_030040C0 + a1 * 8;  //TODO:Same above.
-    v2 = (u32*)v1;
-    return v2[3];
+    struct Unk_030040C0 *var = &gUnk_030040C0[a1];
+
+    return var->unkC;
 }
