1c5fba14113629c37db14a0fc8c4f95cad453efb
Cofflag <2690470152@qq.com>
2023-04-12 12:46:41 +0800

    new file

---
+++ include/ramptr.h
@@ -0,0 +1,19 @@
+#ifndef GUARD_RAMPTR_H
+#define GUARD_RAMPTR_H
+
+#include "global.h"
+
+extern u32 gUnk_03006590[];
+extern u32 *gUnk_030067B0;
+extern u16 *gUnk_0300657C;
+extern u16 gUnk_02000100[];
+extern u8 *gUnk_03006584;
+extern u8 gUnk_02000380[];
+extern u16 *gUnk_030067AC;
+extern u16 gUnk_02000540[];
+extern u8 *gUnk_03006570;
+extern u8 gUnk_02000580[];
+extern u8 *gUnk_030067A8;
+extern u8 gUnk_02000680[];
+
+#endif
---
+++ src/ramptr.c
@@ -0,0 +1,17 @@
+#include "global.h"
+#include "ramptr.h"
+#include "data.h"
+#include "functions.h"
+
+void sub_08012C44(void)
+{
+    CpuFill16(0, gUnk_03006590, 0x218);
+    gUnk_030067B0 = (u32*)EWRAM_START;
+    gUnk_0300657C = gUnk_02000100;
+    gUnk_03006584 = gUnk_02000380;
+    gUnk_030067AC = gUnk_02000540;
+    gUnk_03006570 = gUnk_02000580;
+    gUnk_030067A8 = gUnk_02000680;
+    CpuFill32(0, (u32*)EWRAM_START, 0x400);
+    CpuFill32(0, gUnk_02000540, 0x1E4);
+}
