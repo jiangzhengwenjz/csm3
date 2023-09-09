bc648e70aac7ed69202d38b2ac179051b36cd1ca
Cofflag <2690470152@qq.com>
2023-09-09 18:38:08 +0800

    non matching

--- src/ramptr.c
+++ src/ramptr.c
@@ -54,75 +54,17 @@ bool32 sub_08012DE0(u16 r0, u16 r1)
         return FALSE;
 }
 
-#ifdef NONMATCHING
-
 void sub_08012E14(u16 r0, u16 r1, u32 r2)
 {
+    struct Unk_03006590_1 *ptr;
+
     gUnk_03006590[r0].unk2C[r1].unk4 = gUnk_03006590[r0].unk28 + 16 + (r2 & 0xfffe);
     gUnk_03006574 = &(gUnk_03006590[r0].unk2C[r1]);
-    gUnk_03006590[r0].unk2C[r1].unk2 = *((u16*)gUnk_03006590[r0].unk2C[r1].unk4);
-    gUnk_03006590[r0].unk2C[r1].unk4 += 1;
+    ptr = &gUnk_03006590[r0].unk2C[r1];
+    ptr->unk2 = *(u16 *)ptr->unk4;
+    ptr->unk4 += 2;
     sub_080127E4();
-    // Not matching yet. 
 }
-
-#else
-
-void __attribute__((naked)) sub_08012E14(u16 r0, u16 r1, u32 r2)
-{
-    asm("\n\
-    .syntax unified\n\
-    push {r4, r5, lr}\n\
-    lsls r0, r0, #0x10\n\
-    lsrs r0, r0, #0x10\n\
-    lsls r1, r1, #0x10\n\
-    lsrs r1, r1, #0x10\n\
-    ldr r3, _08012E6C @ =gUnk_03006590\n\
-    mov ip, r3\n\
-    lsls r5, r1, #3\n\
-    subs r5, r5, r1\n\
-    lsls r5, r5, #2\n\
-    lsls r1, r0, #4\n\
-    adds r1, r1, r0\n\
-    lsls r1, r1, #2\n\
-    subs r1, r1, r0\n\
-    lsls r1, r1, #2\n\
-    adds r4, r5, r1\n\
-    mov r0, ip\n\
-    adds r0, #0x30\n\
-    adds r4, r4, r0\n\
-    subs r0, #8\n\
-    adds r0, r1, r0\n\
-    ldr r3, _08012E70 @ =0x0000FFFE\n\
-    ands r3, r2\n\
-    adds r3, #0x10\n\
-    ldr r0, [r0]\n\
-    adds r0, r0, r3\n\
-    str r0, [r4]\n\
-    ldr r2, _08012E74 @ =gUnk_03006574\n\
-    mov r0, ip\n\
-    adds r0, #0x2c\n\
-    adds r1, r1, r0\n\
-    adds r1, r1, r5\n\
-    str r1, [r2]\n\
-    ldr r0, [r1, #4]\n\
-    ldrh r2, [r0]\n\
-    strh r2, [r1, #2]\n\
-    adds r0, #2\n\
-    str r0, [r1, #4]\n\
-    bl sub_080127E4\n\
-    pop {r4, r5}\n\
-    pop {r0}\n\
-    bx r0\n\
-    .align 2, 0\n\
-_08012E6C: .4byte gUnk_03006590\n\
-_08012E70: .4byte 0x0000FFFE\n\
-_08012E74: .4byte gUnk_03006574\n\
-        .syntax divided\n\
-    ");
-}
-
-#endif
 
 void *sub_08012E78(u16 r0, u16 r1)
 {
