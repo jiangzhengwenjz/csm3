164379116c3794fe9c8275d07f51f65726b37f4f
Cofflag <2690470152@qq.com>
2024-02-25 16:55:07 +0800

    08004614

--- src/codec_08004544.c
+++ src/codec_08004544.c
@@ -46,39 +46,10 @@ void sub_080045E4(u16 r0, u16 r1)
     sub_08010D40(temp.unk14, r0, r1);
 }
 
-#ifdef NONMATCHING
-
-u16 sub_08004614(u32 r0, u32 r1, u32 r2, u16 r3, u16 sp10, u32 sp14, u32 sp18)
+u16 sub_08004614(u32 r0, u32 r1, u32 r2, u32 r3, u32 sp10, u32 sp14, u32 sp18)
 {
-    return r0 | r1 | r2 | sp14 | sp18 | (r3 << 8 | sp10 << 2);
+    u32 v1 = r2 | (r1 | r0);
+    r3 <<= 0x10;
+    sp10 <<= 0x10;
+    return  v1 | sp14 | sp18 | (r3 >> 8 | sp10 >> 14);
 }
-
-#else
-
-__attribute__((naked)) u16 sub_08004614(u32 r0, u32 r1, u32 r2, u16 r3, u16 sp10, u32 sp14, u32 sp18)
-{
-    asm(".syntax unified");
-    asm("push {r4, r5, r6, lr}");
-    asm("ldr r5, [sp, #0x10]");
-    asm("ldr r4, [sp, #0x14]");
-    asm("ldr r6, [sp, #0x18]");
-    asm("orrs r1, r0");
-    asm("orrs r2, r1");
-    asm("lsls r3, r3, #0x10");
-    asm("lsls r5, r5, #0x10");
-    asm("orrs r4, r2");
-    asm("orrs r6, r4");
-    asm("lsrs r3, r3, #8");
-    asm("lsrs r5, r5, #0xe");
-    asm("orrs r3, r5");
-    asm("orrs r3, r6");
-    asm("lsls r3, r3, #0x10");
-    asm("lsrs r3, r3, #0x10");
-    asm("adds r0, r3, #0");
-    asm("pop {r4, r5, r6}");
-    asm("pop {r1}");
-    asm("bx r1");
-    asm(".syntax divided");
-}
-
-#endif
