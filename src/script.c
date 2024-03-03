f4a61b6a46879c0b41031633421237a29d6edeeb
Cofflag <2690470152@qq.com>
2024-03-03 13:06:13 +0800

    more script

--- src/script.c
+++ src/script.c
@@ -1,9 +1,60 @@
 #include "data.h"
 #include "ramptr.h"
 
+extern u32 (*gUnk_08B716B4[])(void);
+extern u32 (*gUnk_08B716F4[])(void);
+extern u32 (*gUnk_08B71750[])(void);
+extern u32 (*gUnk_08B718B0[])(void);
+extern u32 (*gUnk_08B71AEC[])(void);
+
 void sub_0801309C(void)
 {
     gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
     sub_080127E4();
     gUnk_03006578->unk1 = 1;
+}
+
+void sub_080130C4(void)
+{
+    if(gUnk_08B716B4[(u8)gUnk_03006574->unk2]() << 0x10 == 0)
+    {
+        gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
+        sub_080127E4();
+    }
+}
+
+void sub_080130FC(void)
+{
+    if(gUnk_08B716F4[(u8)gUnk_03006574->unk2]() << 0x10 == 0)
+    {
+        gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
+        sub_080127E4();
+    }
+}
+
+void sub_08013134(void)
+{
+    if(gUnk_08B71750[(u8)gUnk_03006574->unk2]() << 0x10 == 0)
+    {
+        gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
+        sub_080127E4();
+    }
+}
+
+void sub_0801316C(void)
+{
+    if(gUnk_08B718B0[(u8)gUnk_03006574->unk2]() << 0x10 == 0)
+    {
+        gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
+        sub_080127E4();
+    }
+}
+
+void sub_080131A4(void)
+{
+    if(gUnk_08B71AEC[(u8)gUnk_03006574->unk2]() << 0x10 == 0)
+    {
+        gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
+        sub_080127E4();
+    }
 }
