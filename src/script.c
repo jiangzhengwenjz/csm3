d271f32fd11894278e3127d3aa933f80164bd7a7
Cofflag <2690470152@qq.com>
2024-03-09 22:48:45 +0800

    more script

--- src/script.c
+++ src/script.c
@@ -7,6 +7,8 @@ extern u32 (*gUnk_08B71750[])(void);
 extern u32 (*gUnk_08B718B0[])(void);
 extern u32 (*gUnk_08B71AEC[])(void);
 
+extern void sub_08001AEC(s16 unk);
+
 void sub_0801309C(void)
 {
     gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
@@ -57,4 +59,55 @@ void sub_080131A4(void)
         gUnk_03006574->unk2 = *gUnk_03006574->unk4++;
         sub_080127E4();
     }
+}
+
+u32 sub_080131DC(void)
+{
+    u16 r2 = *gUnk_03006574->unk4;
+    u32 r4;
+    gUnk_03006574->unk4++;
+    r4 = r2 << 0x10;
+    if (sub_08012578() == 0)
+    {
+        gUnk_03006574->unk4 = (r4 >> 0x11 << 1) + 0x10 + gUnk_03006578->unk28; // TODO: I think this is a fakematch.
+    }
+    gUnk_03006578->unk1 = 1;
+    return 0;
+}
+
+u32 sub_08013220(void)
+{
+    u16 r1 = *gUnk_03006574->unk4++;
+    gUnk_03006574->unk4 = (r1 >> 1 << 1) + 0x10 + gUnk_03006578->unk28;
+    gUnk_03006578->unk1 = 1;
+    return 0;
+}
+
+u32 sub_0801324C(void)
+{
+    gUnk_03006590[0].unk2C[sub_08012578()].unk0 = 9;
+    gUnk_03006578->unk1 = 1;
+    return 0;
+}
+
+u32 sub_08013278(void)
+{
+    u32 t1 = sub_08012578();
+    u8 t2 = gUnk_03006590[t1].unk0;
+    gUnk_030067B0[0x7] = t2;
+    gUnk_03006578->unk1 = 1;
+    return 0;
+}
+
+u32 sub_080132B0(void)
+{
+    return 0;
+}
+
+u32 sub_080132B4(void)
+{
+    u32 r0 = 0;
+    gUnk_030067B0[4] = r0;
+    sub_08001AEC(r0); // is this UB?
+    return 0;
 }
